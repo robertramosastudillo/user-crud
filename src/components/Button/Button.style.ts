42022551099d5c538b78e82cac6fba6656dc78c4
robertramosastudillo <robertramosastudillo@gmail.com>
2022-01-20 22:08:47 -0500

    Add button styling

--- src/components/Button/Button.style.ts
+++ src/components/Button/Button.style.ts
@@ -3,7 +3,22 @@ import styled from "styled-components";
 // import { FiSearch } from "react-icons/fi";
 
 export const ButtonStyle = styled.button`
-  background-color: steelblue;
   width: 100%;
+  padding: 1rem;
+  font-size: 16px;
+  border-radius: 0.8rem;
+  height: 45px;
+  font-weight: bold;
+  background-color: steelblue;
   color: white;
+  outline: none;
+  border: none;
+  cursor: pointer;
+  margin: 0 0 2rem 0;
+
+  &:hover {
+    background-color: darken(#FF5151, 4);
+    box-shadow: 0px 0px 16px darken(#FF5151, 4);
+  }
+
 `;
