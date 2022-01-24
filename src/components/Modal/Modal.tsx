a47db34a972afb33291a82ae5d47a9337b432970
robertramosastudillo <robertramosastudillo@gmail.com>
2022-01-23 20:45:51 -0500

    modal component created

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,4 @@
+
+export const Modal = () => {
+  return <div>Modal</div>;
+};
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
+export { Modal } from "./Modal/Modal";
 export { Button } from "./Button/Button";
 export { SearchBox } from "./SearchBox/SearchBox";
 export { Table } from "./Table/Table";
